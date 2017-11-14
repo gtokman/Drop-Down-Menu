804cf3f74a4253f9fe92c7085df0b63ffa7e7f28
Gary Guerman Tokman <gtokman@fullsail.edu>
2017-11-14 18:52:03 -0500

    finished app

--- DropDown/ViewController.swift
+++ DropDown/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cityButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func handleSelection(_ sender: UIButton) {
+        cityButtons.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    enum Citys: String {
+        case boston = "Boston"
+        case sanFranscisco = "San Francisco"
+    }
+    
+    @IBAction func cityTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle, let city = Citys(rawValue: title) else {
+            return
+        }
+        
+        switch city {
+        case .boston:
+            print("Boston")
+        default:
+            print("San fran")
+        }
+        
+    }
 }
 
